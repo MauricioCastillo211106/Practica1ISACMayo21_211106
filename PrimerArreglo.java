44c7a6e826f765fa4074262d2669d7f725744636
MauricioCastillo211106 <211106@ids.upchiapas.edu.mx>
2021-05-25 11:23:49 -0500

    Primera Version

---
+++ PrimerArreglo.java
@@ -0,0 +1,9 @@
+public class PrimerArreglo {
+    public static void main(String[] args) {
+        int arreglo [] = new int [4];
+        for (int i=0; i<=3; i++){
+            arreglo[i]=i*2;
+            System.out.println(arreglo[i]);
+        }
+    }
+}
